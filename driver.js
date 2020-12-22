0ab4b199e5cde9bc5b8784818dd6adbf82eb0f2d
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2020-12-21 19:38:27 -0700

    all tests written and passing

--- driver.js
+++ driver.js
@@ -7,14 +7,30 @@ events.on('pickup', delivered);
 
 function inTransit (payload) {
   setTimeout(() => {
-    console.log(`DRIVER: picked up ${payload.orderID}`);
     events.emit('in-transit', payload);
   }, 1000);
 }
 
+events.on('in-transit', driverPickup);
+
+function driverPickup (payload) {
+  console.log(`DRIVER: picked up ${payload.orderID}`);
+}
+
 function delivered (payload) {
   setTimeout(() => {
-    console.log(`DRIVER: delivered ${payload.orderID}`);
+    // console.log(`DRIVER: delivered ${payload.orderID}`);
     events.emit('delivered', payload);
   }, 3000);
 }
+
+events.on('delivered', driverDelivered);
+
+function driverDelivered (payload) {
+  console.log(`DRIVER: delivered ${payload.orderID}`);
+}
+
+
+
+
+module.exports = { inTransit, delivered, driverPickup };
